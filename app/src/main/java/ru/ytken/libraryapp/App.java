5124db7bd5217097da4d45bd95b6024cb4896a39
ytken <1909dande.lion@gmail.com>
2021-03-19 04:59:08 +0500

    Initial commit

---
+++ app/src/main/java/ru/ytken/libraryapp/App.java
@@ -0,0 +1,17 @@
+package ru.ytken.libraryapp;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+import java.util.concurrent.TimeUnit;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // cold launches take some time
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(8));
+    }
+}
